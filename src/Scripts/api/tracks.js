81dc654b8d1789923bb71b486cade43e17cb04b5
James J Regan IV <rpgprog@gmail.com>
2013-08-11 21:22:52 -0400

    Working out the track Api format

---
+++ src/Scripts/api/tracks.js
@@ -0,0 +1,123 @@
+﻿[
+    { 
+        "id": "Rage",
+        "name": "Rage",
+        "komOverride": "Str",
+        "type": "barbarian",
+        "circles": [
+            { "name": "Rage", "text": "You channel the barbarian rage, a state where you gain heightened morale and physical prowess. As a swift action, if you are not [Fatigued], [Exhausted], or in a rage, you can enter a rage, which lasts for a maximum number of [Rounds] equal to 3 + your Constitution modifier (if positive). While in a rage, you gain a +1 bonus per circle you possess from this track to attack rolls and damage you deal with attacks, and a +1 bonus to combat maneuver DCs and Fortitude and Will saves. When you enter a rage, you also gain 2 temporary HP per level. These temporary HP are lost when your rage ends. You can always prematurely end your rage as a free action. When your rage ends (either before the beginning of your turn due to its duration running out, or on your turn due to your ending it prematurely), you become [Fatigued] for as many [Rounds] as you spent raging (minimum 1), and you cannot begin a rage on either the turn it ends or on your following turn." },
+            { "name": "Powerful Rage", "text": "Your rage gives you unusual physical ability equal to a creature much larger than yourself. You gain a +2 item bonus to Strength. While in a rage, your size becomes [Large] (if you are not already [Large] or [Huge], you gain the normal +5 ft bonus to your movement speed, +2 bonus to saves against combat maneuvers, and 5 ft bonus to your [Melee] range associated with the [Large] size). While in a rage, you do not suffer any of the normal penalties of being [Large], even if you’re normally [Large]." },
+            { "name": "Intimidating Rage", "text": "Once per [Encounter], when you enter a rage, you can attempt to intimidate a single opponent within 30 ft as a free action (as per the in-combat use of the Intimidate skill). If your intimidate attempt succeeds, that opponent becomes [Shaken] until the end of the [Encounter]. In addition, your bonus to combat maneuver DCs while raging increases to +2." },
+            {
+                "name": "Greater Rage",
+                "text": "Your rage increases in potency. You gain 4 temporary HP per level when you rage instead of 2, and your bonuses to Fortitude and Will saves from Rage increase to +2.",
+                "choice": [
+                    { "name": "Hurling Charge", "text": "While raging, after making a melee attack as part of the Charge combat maneuver, you can use a ranged weapon in your possession to make a single ranged attack. This does not provoke an attack of opportunity. You can treat any square you moved into as part of the Charge combat maneuver as a square you occupy for purposes of the range, line of effect and line of sight of this attack." },
+                    { "name": "Momentum Charge", "text": "While raging, if you use the Charge combat maneuver and your melee attack hits, you may immediately attempt the Bull Rush combat maneuver against the opponent hit. If your Bull Rush attempt succeeds, you may deal damage equal to your Key Offensive Modifier to that opponent." }
+                ]
+            },
+            { "name": "Stubborn Rage", "text": "While in a rage, you possess [Immunity] to effects with the [Mind-affecting] descriptor. In addition, your bonus to combat maneuver DCs while raging increases to +3." },
+            { "name": "Mighty Rage", "text": "The power of your rage increases again. You gain 6 temporary HP per level when you rage instead of 4, and the bonus to Fortitude and Will saves increases to +4." },
+            { "name": "Heart of Fury", "text": "You are always considered “raging.” You can activate the Intimidating Rage ability once per [Encounter] as a swift action. At the beginning of an [Encounter], you gain temporary HP (as per Mighty Rage), and no less than 2 [Rounds] after gaining temporary HP from this ability, you may gain a new set of temporary HP as a swift action." }
+        ]
+    },
+    {
+        "id": "dervish",
+        "name": "Dervish",
+        "komOverride": "Dex",
+        "type": "barbarian",
+        "circles": [
+            { "name": "War Dance", "text": "You can whip yourself into a war dance, a state in which you gain heightened morale and maneuverability. As a swift action, if you are not [Fatigued], [Exhausted], or in a war dance, you can enter a war dance, which lasts for a maximum number of [Rounds] equal to 3 + your Constitution modifier (if positive). While in this war dance you gain a +1 bonus to attack rolls, damage you deal with attacks, and Acrobatics checks per circle you possess from this track, a +1 bonus to Reflex and Will saves, and a +2 bonus to saves against combat maneuvers. You can always prematurely end your war dance as a free action.When your war dance ends (either before the beginning of your turn due to its duration running out, or on your your turn due to your ending it prematurely) you cannot begin a war dance on either the turn it ends or on your following turn. In addition, you are [Fatigued] for as many [Rounds] as you spent war dancing (minimum 1)." },
+            { "name": "Frenzied Strikes", "text": "Your frantic movements become more focused and lethal. While in a war dance, you gain a [Bonus attack] whenever you take an attack action and you gain a 5 ft bonus to your movement speed, which increases to 10 ft at 4th circle and 15 ft at 6th circle." },
+            { "name": "Razor Wind", "text": "Your whirling attacks amaze friend and foe alike, leaving vicious wounds at blinding speed. You gain a +2 bonus to Acrobatics checks. The first hit with an attack you make against each opponent each [Round] inflicts the [Burning] and [Bleeding] conditions on that opponent." },
+            {
+                "name": "Greater War Dance",
+                "text": "Your war dance increases in potency, and you become adept at avoiding the sharp pointy bits of metal other people want to stick in you. Your bonuses to Reflex and Will saves while in a war dance increase to +2. While in a war dance, you gain 20% [Miss chance]. ",
+                "choice": [
+                    { "name": "Cloaked Dance", "text": "While in a war dance, you can use the Surprising an Opponent function of the Acrobatics skill as part of a move action without halving your movement speed. Additionally, whenever you deal damage to a [Flat-footed] opponent, you deal additional damage equal to your KOM." },
+                    { "name": "Tendon Sever", "text": "While in a war dance, any time you hit a [Flat-footed] opponent with an attack, you inflict the [Entangled] condition on that opponent for one [Round]." }
+                ]
+            },
+            { "name": "Unstoppable War Dance", "text": "While in a war dance, you gain [Immunity] to [Binding] effects, and moving out of a square of difficult terrain does not cost you extra movement." },
+            { "name": "Whirling War Dance", "text": "Your war dance has reached the point where you are little more than a terrifying tempest of blades. Your bonuses to Reflex and Will saves while in a war dance increase to +4. Your ability to avoid damage increases as well, granting a 30% [Miss chance] while you are in a war dance." },
+            { "name": "Dance of the Endless Blades", "text": "Your war dance is a primal thing of terrible wrath and frightening beauty. Any time you hit a [Bleeding] opponent with an attack, they are [Battered] for the [Encounter].Additionally, while in a war dance, as a move action and a standard action, you move up to twice your movement speed without provoking attacks of opportunity. You may take an attack action during this movement, and may divide all of the attacks you are entitled to among any targets who were within [Melee] range at any point during your movement. Finally, you may enter your war dance as a free action." }
+        ]
+    },
+    {
+        "id": "pathOfDestruction",
+        "name": "Path Of Destruction",
+        "type": "barbarian",
+        "circles": [
+            { "name": "Cleave", "text": "Once per [Round], as a swift action, when you hit an opponent with a melee attack and deal damage to that opponent with that attack, you may compare your attack roll from that attack to the AC of another target opponent within [Melee] range. If the attack roll equals or exceeds that opponent’s AC, you deal that much damage to that opponent." },
+            { "name": "Whirlwind", "text": "If you would make one or more melee attacks during your standard action, you may add the [Volley] descriptor to a single one of those attacks; this attack with the [Volley] descriptor is applied against every opponent in [Melee] range." },
+            { "name": "Disrupting Presence", "text": "You are a living force of entropy, and creatures who attempt mental focus close to you suffer the consequences. Any opponent who activates a spell, spell-like, or supernatural ability within your [Melee] range takes damage equal to your character level. " },
+            { "name": "Terrifying Presence", "text": "At the beginning of your turn, opponents within your [Melee] range must make a Will save (DC 10 + ½ your level + your Charisma modifier) every [Round] or become [Shaken]. This application of the [Shaken] condition doesn’t stack with itself, but stacks normally with other [Fear] effects. This is a [Mindaffecting] [Fear] effect." },
+            { "name": "Path of Blades", "text": "When you use the Charge maneuver, you can make a single additional attack with the [Volley] descriptor as you make the Charge; this attack with the [Volley] descriptor is applied to all opponents within [Melee] range of squares you move into as part of the Charge. This attack deals damage equal to three times your KOM." },
+            { "name": "Greater Cleave", "text": "Activating Cleave no longer requires a swift action, and it can be used any number of times per [Round]." },
+            { "name": "Deadly Presence", "text": "When an opponent begins their turn within your [Melee] range, it takes damage equal to your level + your Key Offensive Modifier and is [Battered] for one [Round]. This ability can be turned on or off as a swift action." },
+        ]
+    },
+    {
+        "id": "pathOfTheAncestors",
+        "name": "Path of the Ancestors",
+        "type": "barbarian",
+        "circles": [
+            {
+                "name": "Lineage",
+                "text": "Any effect that would heal you but does not originate from you heals 1 extra point of damage for every level you possess.",
+                "choice": [
+                    { "name": "Guidance", "text": "You gain a bonus equal to half your level (minimum 1) on initiative rolls." },
+                    { "name": "Leading the Charge", "text": "When using the Charge combat maneuver, you ignore the normal -2 penalty to Armor Class." }
+                ]
+            },
+            { "name": "Hard to Break", "text": "Once per [Encounter], if you make a successful Fortitude or Will save against an effect that would normally have a lesser effect on a successful save (such as a spell with a save of “Fortitude half ”), you instead ignore the effect entirely." },
+            { "name": "Readiness", "text": "You gain a +2 bonus to Vigor checks, and becoming [Flat-footed] does not prevent you from making attacks of opportunity or taking immediate actions. Once per [Round], you may make a Vigor check at any time during the [Round]. This does not require an action." },
+            { "name": "Resilience", "text": "You gain [Fast healing] equal to your character level." },
+            { "name": "Ancestral Bleeding", "text": "Once per [Encounter], as an immediate action, you can reroll a failed save." },
+            { "name": "Greater Readiness", "text": "You are always able to act in a surprise [Round]." },
+            { "name": "Strength Unbroken", "text": "Once per [Encounter], as a swift action, you can begin an unstoppable assault. Until the beginning of your next turn, you gain [Immunity] to damage, [Binding] effects, and the [Dazed] and [Stunned] conditions." }
+        ]
+    },
+    {
+        "id": "celestial",
+        "name": "Celestial",
+        "type": "racial",
+        "circles": [
+            { "name": "Divine Radiance", "text": "At will, as a free action, if you do not already have a radiant spread, you may create a radiant spread with a radius of your [Close] originating from you that moves with you and lasts until dismissed as a swift action. This spread causes opponents in it to lose the [Covered] condition and be [Revealed]. Any opponent who leaves the spread is [Revealed] for one [Round] and cannot benefit from the [Covered] condition for one [Round]. As a swift action, you may increase the light’s brightness momentarily, leaving all opponents within the radiant spread [Dazzled] for 5 [Rounds]. A successful Fortitude save (DC 10 + ½ your level + your Wisdom modifier) reduces the duration of the [Dazzled] condition to 1 [Round]." },
+            { "name": "Sway the Heavens", "text": "Your attacks awe opponents with their wondrous grace. Once per [Round], if you hit an opponent that has been [Revealed] by your Divine Radiance with an attack, you may cause that opponent to be unable to make attacks of opportunity for one [Round]. This does not require an action. You may activate this ability an additional time per [Round] for every circle of Celestial you possess above 2nd." },
+            {
+                "name": "Path to Glory",
+                "text": "",
+                "choice": [
+                    { "name": "Angelic Wings", "text": "You gain a pair of wings and the Fly movement mode." },
+                    { "name": "Prophetic Vision", "text": "Brief glimpses into the future offer you keen insight into battle. Once per [Round], when you activate Divine Radiance as a swift action, you may gain a +3 bonus to attack rolls or Armor Class until the beginning of your next turn." }
+                ]
+            },
+            {
+                "name": "Benediction",
+                "text": "",
+                "choice": [
+                    { "name": "Strength of Fate", "text": "Twice per [Encounter], you can activate the Divine Radiance ability as an immediate action, and replace the Armor Class or one saving throw bonus of an ally within the range of your Divine Radiance with your own for one [Round]. When you do so, you may benefit from other abilities you possess that trigger when you activate Divine Radiance as a swift action." },
+                    { "name": "Aura of Vitality", "text": "You gain [Immunity] to the [Blinded] condition. In addition, twice per [Encounter], when you activate Divine Radiance as a swift action, you may grant all allies within the range of your Divine Radiance 2 temporary HP per level you possess." }
+                ]
+            },
+            {
+                "name": "Harmonious Scales",
+                "text": "",
+                "choice": [
+                    { "name": "Purifying Flame", "text": "Twice per [Encounter], as a standard action, you may use sunburst, as the spell. The DC to resist it is (10 + ½ your level + your Wisdom modifier)." },
+                    { "name": "Cleansing Light", "text": "Twice per [Encounter], as a standard action, you may use heal, as the spell. The DC to resist it is (10 + ½ your level + your Wisdom modifier)." }
+                ]
+            },
+            {
+                "name": "Righteous Wrath",
+                "text": "",
+                "choice": [
+                    { "name": "Strike Senseless", "text": "The first time you hit an opponent with an attack each [Round], that opponent is [Blinded] and [Deafened] for 1 [Round]. A successful Fortitude save (DC 10 + ½ your level + your Wisdom modifier) negates the [Blinded] and [Deafened] conditions." },
+                    { "name": "That Hideous Strength", "text": "Once per [Round], when you make an attack, you may force all enemies within the area of your Divine Radiance to make a Will save (DC 10 + ½ your level + your Wisdom modifier) or be [Battered] for 2 [Rounds]." }
+                ]
+            },
+            { "name": "Divine Mercy", "text": "Once per [Encounter], when you activate Divine Radiance as a swift action, you call down a shield of blistering light which grants a single ally within [Long] range [Immunity] to damage for a single [Round]." }
+        ]
+    }
+]
